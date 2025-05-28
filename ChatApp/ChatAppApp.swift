be264b80066c38a80cf9d274e101bf2be22a2e2c
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-05-28 16:16:11 +0530

    initial commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Sandeep Sahani on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
