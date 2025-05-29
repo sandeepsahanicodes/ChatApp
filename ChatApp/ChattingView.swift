4478a1846b5838dd9bb49b52f87156bef2d490ee
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-05-29 15:01:20 +0530

    fix: closes #1; issue resolve regarding navigation to chatting view

--- ChatApp/ChattingView.swift
+++ ChatApp/ChattingView.swift
@@ -8,20 +8,41 @@
 import SwiftUI
 
 struct ChattingView: View {
+    let chat: Chat
     let messages: [Message]
     
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        NavigationStack {
-            List(messages) { message in
-                MessageBubbleView(message: message.message, messageSendTime: message.messageSendTime, messageType: message.messageType, isMessageReceived: message.isMessageReceived)
-                    .listRowSeparator(.hidden)
-            }
-            .navigationTitle("Hee")
-            .listStyle(.plain)
+        List(messages) { message in
+            MessageBubbleView(message: message.message, messageSendTime: message.messageSendTime, messageType: message.messageType, isMessageReceived: message.isMessageReceived).listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                            Button {
+                              dismiss()
+                            } label: {
+                                Image(systemName: "chevron.left")
+                            }
+                        }
+            ToolbarItem(placement: .navigation) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                        Text(chat.userName)
+                            .font(.callout)
+                            .fontWeight(.semibold)
+                        
+                    }
+                }
         }
     }
 }
 
 #Preview {
-    ChattingView(messages: MockMessageData.mockMessages)
+    ChattingView(chat: MockData.chatList[1], messages: MockMessageData.mockMessages)
 }
