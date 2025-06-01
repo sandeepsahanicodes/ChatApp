b52f8851568c95852067ae45ba17f8764acf97df
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-06-01 17:19:54 +0530

    feat(ui): implement authentication screens

--- ChatApp/Screens/Authentication/SignInView/SignInView.swift
+++ ChatApp/Screens/Authentication/SignInView/SignInView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                VStack {
+                    Image(systemName: "message.circle.fill")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                    Text("ChatApp")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }
+                .padding(.bottom, 30)
+                
+                VStack(spacing: 20) {
+                    InputView(text: $email, title: "Email Address", placeholder: "Enter your email")
+                        .textInputAutocapitalization(.never)
+                    
+                    InputView(text: $password, title: "Password", placeholder: "Enter your password")
+                }
+                .padding(.horizontal)
+               
+            }
+            .padding(.top, 50)
+            
+            Button {
+                print("Login user")
+            } label: {
+                RoundedButton(text: "Sign In", backgroundColor: .blue)
+            }.padding(.top, 30)
+            
+            Spacer()
+            
+            NavigationLink {
+                SignUpView()
+                    .navigationBarBackButtonHidden()
+            } label: {
+                HStack {
+                    Text("Don't have an account?")
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                }
+            }
+        }
     }
 }
 
--- ChatApp/Screens/Authentication/SignUpView/SignUpView.swift
+++ ChatApp/Screens/Authentication/SignUpView/SignUpView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var fullName: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Image(systemName: "message.circle.fill")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                Text("ChatApp")
+                    .font(.title)
+                    .fontWeight(.semibold)
+            }
+            .padding(.top, 50)
+            .padding(.bottom, 30)
+            
+            VStack(spacing: 20) {
+                InputView(text: $fullName, title: "Full Name", placeholder: "Enter your name")
+                    .textInputAutocapitalization(.words)
+                
+                InputView(text: $email, title: "Email Address", placeholder: "Enter your email")
+                    .textInputAutocapitalization(.never)
+                
+                InputView(text: $password, title: "Password", placeholder: "Enter your password", isSecured: true)
+                
+                InputView(text: $confirmPassword, title: "Confirm Password", placeholder: "Enter your password", isSecured: true)
+            }
+            .padding(.horizontal)
+            
+            Button {
+                print("Register the user")
+            } label: {
+                RoundedButton(text: "Sign Up", backgroundColor: .blue)
+            }.padding(.top, 30)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack {
+                    Text("Already have an account?")
+                    Text("Sign In")
+                        .fontWeight(.semibold)
+                }
+            }
+            
+        }
+        
     }
 }
 
